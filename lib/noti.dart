d88180e28571b128daf1dad9a27ca357f4caa09d
nghkh <khoa.bach01@gmail.com>
2022-08-09 14:21:09 +0700

    upadate

--- lib/noti.dart
+++ lib/noti.dart
@@ -44,70 +44,68 @@ class _NotiState extends State<Noti> {
                   scrollDirection: Axis.vertical,
                   shrinkWrap: true,
                   itemBuilder: (context, index) {
-                    return Container(
-                      child: Row(
-                        children: [
-                          Stack(
-                            children: [
-                              ClipRRect(
-                                borderRadius: BorderRadius.circular(28.0),
+                    return Row(
+                      children: [
+                        Stack(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(28.0),
+                              child: Image.network(
+                                '${notificationResponse.data![index].image}',
+                                width: 56,
+                                height: 56,
+                              ),
+                            ),
+                            Positioned(
+                              bottom: 0,
+                              right: 0,
+                              child: Container(
+                                width: 24,
+                                height: 24,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(14),
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              bottom: 1.5,
+                              right: 1.5,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(12),
                                 child: Image.network(
-                                  '${notificationResponse.data![index].image}',
-                                  width: 56,
-                                  height: 56,
+                                  '${notificationResponse.data![index].icon}',
+                                  width: 20,
+                                  height: 20,
                                 ),
                               ),
-                              Positioned(
-                                bottom: 0,
-                                right: 0,
-                                child: Container(
-                                  width: 24,
-                                  height: 24,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(14),
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
-                              Positioned(
-                                bottom: 1.5,
-                                right: 1.5,
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(12),
-                                  child: Image.network(
-                                    '${notificationResponse.data![index].icon}',
-                                    width: 20,
-                                    height: 20,
-                                  ),
-                                ),
-                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          width: 4,
+                        ),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                  '${notificationResponse.data![index].message?.text}'),
+                              Text(DateTime.fromMicrosecondsSinceEpoch(
+                                      notificationResponse
+                                              .data![index].createdAt! *
+                                          1000)
+                                  .toString()),
                             ],
                           ),
-                          const SizedBox(
-                            width: 4,
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.more_horiz,
                           ),
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                    '${notificationResponse.data![index].message?.text}'),
-                                Text(DateTime.fromMicrosecondsSinceEpoch(
-                                        notificationResponse
-                                                .data![index].createdAt! *
-                                            1000)
-                                    .toString()),
-                              ],
-                            ),
-                          ),
-                          IconButton(
-                            onPressed: () {},
-                            icon: const Icon(
-                              Icons.more_horiz,
-                            ),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     );
                   },
                   itemCount: notificationResponse.data!.length,
